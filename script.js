79be2d6208da5837976294ee91538740f8ea3825
sockly <granttburnett@gmail.com>
2026-07-09 15:06:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-let hp = 42;
-
-function applyDamage() {
-    const damage =
-        parseInt(document.getElementById("damage").value) || 0;
-
-    hp -= damage;
-
-    if (hp < 0) {
-        hp = 0;
-    }
-
-    document.getElementById("hp").textContent = hp;
-}
+let party = [
+  {
+    id: 1,
+    name: "Rowan",
+    hp: {
+      current: 42,
+      max: 42
+    },
+    ac: 17,
+    initiative: 4,
+    resources: [
+      {
+        name: "Spell Slots",
+        current: 3,
+        max: 4
+      },
+      {
+        name: "Second Wind",
+        current: 1,
+        max: 1
+      }
+    ],
+    customFields: []
+  }
+];
