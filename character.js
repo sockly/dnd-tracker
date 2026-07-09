ae4bd714eeefb6a742e381f21e8d06b428d87933
sockly <granttburnett@gmail.com>
2026-07-09 15:04:42 -0500

    Create character.js

---
+++ character.js
@@ -0,0 +1,16 @@
+const character = {
+  name: "Rowan",
+  hp: {
+    current: 42,
+    max: 42
+  },
+  ac: 17,
+  initiative: 4,
+  resources: [
+    {
+      name: "Spell Slots",
+      current: 3,
+      max: 4
+    }
+  ]
+};
